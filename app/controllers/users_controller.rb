3df35872540dd5feb4f48559a2ef3249c878df68
ayana0120 <keichan0501.ayana@gmail.com>
2020-08-19 23:36:38 +0900

    [Update]コントローラー更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+
   def show
+  	@user = User.new
   end
 
   def index
   end
 
   def edit
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:name, :profile_image)
   end
 end
