ab465496f4b0be7fd66c4b87e30cd7f968e62f39
ayana0120 <keichan0501.ayana@gmail.com>
2020-08-19 23:34:48 +0900

    [Fix]ログイン機能コード修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root "homes#top"
-  get "/home/about" => "homes#about"
+
   devise_for :users, controllers: {
     sessions: 'devise/sessions',
     registrations: 'devise/registrations'
   }
 
+  get "/home/about" => "homes#about"
+  resources :users
+  resources :books
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
