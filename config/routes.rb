c16e7e7ac468c1f74711b351c844a59049d5a5d3
ayana0120 <keichan0501.ayana@gmail.com>
2020-08-19 17:46:24 +0900

    [Add]top・aboutページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root "homes#top"
+  get "/home/about" => "homes#about"
   devise_for :users, controllers: {
     sessions: 'devise/sessions',
     registrations: 'devise/registrations'
   }
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
