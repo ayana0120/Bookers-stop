2eca55ffd913859271d71301651fb621b9bfc91c
ayana0120 <keichan0501.ayana@gmail.com>
2020-08-22 23:31:56 +0900

    [Update]ルート更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
-  authenticated :user do
-    root :to => "users#show", :as => "user_authenticated_root"
-  end
-
-  root "homes#top"
-
-
   devise_for :users, controllers: {
     sessions: 'devise/sessions',
     registrations: 'devise/registrations'
   }
+  authenticated :user do
+    root :to => "users#show", :as => "user_authenticated_root"
+  end
+  root "homes#top"
 
   get "/home/about" => "homes#about"
-  resources :users
-  resources :books
+  resources :users, only: [:show, :index, :edit, :create, :update]
+  resources :books, only: [:show, :index, :edit, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
