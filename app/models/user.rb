cdbbd16c71cb3e4a7bd25ec96d24aa2e8642fe70
ayana0120 <keichan0501.ayana@gmail.com>
2020-08-22 23:34:41 +0900

    [Fix]カラム名変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  attachment :image
   has_many :books, dependent: :destroy
+  attachment :progile_image
 end
